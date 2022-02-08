ba5532f9684562ac1794718bd2328bdbf9a48340
Igor Dragun <dragun.msc@gmail.com>
2022-02-08 19:04:43 +0300

    init

---
+++ internal/handlers/handlers.go
@@ -0,0 +1,113 @@
+package handlers
+
+import (
+	"fmt"
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
+	"sort"
+)
+
+type WebSocketConnection struct {
+	*websocket.Conn
+}
+
+type WsJsonResponse struct {
+	Action         string      `json:"action"`
+	Message        interface{} `json:"message"`
+	ConnectedUsers []string    `json:"connected_users"`
+}
+
+type WsPayload struct {
+	Action   string              `json:"action"`
+	Username string              `json:"username"`
+	Message  interface{}         `json:"message"`
+	Conn     WebSocketConnection `json:"-"`
+}
+
+var (
+	clients           = make(map[WebSocketConnection]models.Hand)
+	wsChan            = make(chan WsPayload)
+	upgradeConnection = websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+		CheckOrigin:     func(r *http.Request) bool { return true },
+	}
+)
+
+func WsEndpoint(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgradeConnection.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	conn := WebSocketConnection{ws}
+	clients[conn] = ""
+
+	go ListenForWs(&conn)
+}
+
+func ListenForWs(conn *WebSocketConnection) {
+	defer func() {
+		if r := recover(); r != nil {
+			log.Println("err", fmt.Sprintf("#{r}"))
+		}
+	}()
+
+	payload := WsPayload{}
+
+	for {
+		err := conn.ReadJSON(&payload)
+		if err != nil {
+			// do nothing
+		} else {
+			payload.Conn = *conn
+			wsChan <- payload
+		}
+	}
+}
+
+func ListenToWsChannel() {
+	response := WsJsonResponse{}
+
+	for {
+		e := <-wsChan
+		switch e.Action {
+		case "username":
+			clients[e.Conn] = e.Username
+			users := getUserList()
+			response.Action = "list_users"
+			response.ConnectedUsers = users
+			broadcastToAll(response)
+		case "left":
+			delete(clients, e.Conn)
+			users := getUserList()
+			response.Action = "list_users"
+			response.ConnectedUsers = users
+			broadcastToAll(response)
+		case "":
+		}
+	}
+}
+
+func getUserList() []string {
+	var userList []string
+	for _, x := range clients {
+		if x != "" {
+			userList = append(userList, x)
+		}
+	}
+	sort.Strings(userList)
+	return userList
+}
+
+func broadcastToAll(response WsJsonResponse) {
+	for client := range clients {
+		err := client.WriteJSON(response)
+		if err != nil {
+			log.Println("websocket error")
+			_ = client.Close()
+			delete(clients, client)
+		}
+	}
+}
