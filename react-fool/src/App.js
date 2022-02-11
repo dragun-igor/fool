e0e527ca3031a386433a3e10dc7d74fd06393b08
Igor Dragun <dragun.msc@gmail.com>
2022-02-11 19:09:43 +0300

    aaa"

--- react-fool/src/App.js
+++ react-fool/src/App.js
@@ -1,50 +1,25 @@
 import logo from './logo.svg';
-// import React, { useState } from 'react';
-// import { render } from 'react-dom';
-// import { DatePicker, message } from 'antd';
-// import 'antd/dist/antd.css';
-// import './index.css';
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <Menu
-        style={{ width: 256 }}
-        defaultSelectedKeys={['1']}
-        defaultOpenKeys={['sub1']}
-        mode="inline"
-      >
-        <SubMenu key="sub1" title={
-          <span>
-            <span>Navigation One</span>
-          </span>}
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
         >
-          <MenuItemGroup key="g1" title="Item 1">
-            <Menu.Item key="1">Option 1</Menu.Item>
-            <Menu.Item key="2">Option 2</Menu.Item>
-          </MenuItemGroup>
-        </SubMenu>
-      </Menu>
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
 
 export default App;
-
-// const App = () => {
-//   const [date, setDate] = useState(null);
-//   const handleChange = value => {
-//     message.info(`Selected Date: ${value ? value.format('YYYY-MM-DD') : 'None'}`);
-//     setDate(value);
-//   };
-//   return (
-//       <div style={{ width: 400, margin: '100px auto' }}>
-//         <DatePicker onChange={handleChange} />
-//         <div style={{ marginTop: 16 }}>
-//           Selected Date: {date ? date.format('YYYY-MM-DD') : 'None'}
-//         </div>
-//       </div>
-//   );
-// };
-
-// render(<App />, document.getElementById('root'));
